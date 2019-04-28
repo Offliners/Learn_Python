109c4029e7e39acff1d8fb3fb8eaa49eddc4b20b
Offliner <42777325+Offliners@users.noreply.github.com>
2019-04-28 11:36:08 +0800

    Create 007.py

---
+++ code/007.py
@@ -0,0 +1,14 @@
+# the "len" built-in function
+
+list_var = [1,2,3,4,5]
+str_var = "Hello, World!"
+dic_var = {"a":100,"b":200}
+
+print(len(list_var))
+print(len(str_var))
+print(len(dic_var))
+
+# Output:
+# 5
+# 13
+# 2

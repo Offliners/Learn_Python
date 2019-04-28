cd152a6c52aa58da86b9ce6d3192c0e0bbd0125f
Offliner <42777325+Offliners@users.noreply.github.com>
2019-04-28 11:09:52 +0800

    Create 003.py

---
+++ code/003.py
@@ -0,0 +1,16 @@
+# different data type
+# in the same variable
+
+x = 42
+print(x)
+print(type(x))
+
+x = "string"
+print(x)
+print(type(x))
+
+# Output:
+# 42
+# <class 'int'>
+# string
+# <class 'str'>

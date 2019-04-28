e8ff1861c498579e9aa3d6e4609fe0f5c2dc118a
Offliner <42777325+Offliners@users.noreply.github.com>
2019-04-28 11:36:52 +0800

    Create 010.py

---
+++ code/010.py
@@ -0,0 +1,22 @@
+str1 = '''This is a multi-line 
+string. That you can continue
+on another line pressing the 
+Enter key'''
+
+print(str1)
+
+str2 = '''Notice the spaces 
+          on the left. Will they
+          appear in the output?
+       '''
+       
+print(str2)
+
+# Output:
+# This is a multi-line
+# string. That you can continue
+# on another line pressing the
+# Enter key
+# Notice the spaces
+#           on the left. Will they
+#           appear in the output?

231088973d5a5dfc50e02214b1ac302103194e3c
Offliner <42777325+Offliners@users.noreply.github.com>
2019-04-30 23:15:22 +0800

    Create 059.py

---
+++ code/059.py
@@ -0,0 +1,13 @@
+# different variables of
+# variable names
+
+# e.g., for the first name
+
+firstname = "Eric" # OK
+first_name = "Eric" # OK
+firstName = "Eric" # JavaScript?
+FirstName = "Eric" # Java ?
+fiRStNaME = "Eric" # hahaha
+FIRSTNAME = "Eric" # constant ?
+
+_first_name = "Eric" # hidden

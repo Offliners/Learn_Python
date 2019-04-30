af564f788cbc42480bf5f7a64cdb2150119cb0d5
Offliner <42777325+Offliners@users.noreply.github.com>
2019-04-30 16:09:12 +0800

    Create 030.py

---
+++ code/030.py
@@ -0,0 +1,18 @@
+# how to split a string to
+# characters
+
+str = "Hello, World!"
+
+# first, print a character
+print(str[3])
+
+# now, make a list out of a string
+# just convert a type:
+chars = list(str)
+print(type(chars))
+print(chars[3])
+
+# Output:
+# l
+# <class 'list'>
+# l

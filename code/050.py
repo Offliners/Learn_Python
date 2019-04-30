d01b64cb29ca64224d24ca197be9cf1b885e296a
Offliner <42777325+Offliners@users.noreply.github.com>
2019-04-30 21:12:17 +0800

    Create 050.py

---
+++ code/050.py
@@ -0,0 +1,14 @@
+# project euler problem 1 
+# projecteuler.net/problem=1
+# find the sum of all numbers
+# below 1000, which are multiple
+# of 3 or 5(i.e., 3, 6, 5, 9, 15,...)
+
+sum = 0
+for n in range(0,1000):
+    if not(n % 3) or not(n % 5):
+        sum += n
+print(sum)
+
+# Output:
+# 233168

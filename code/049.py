a86c3c3f770acddf86037c75716d858be2b506d2
Offliner <42777325+Offliners@users.noreply.github.com>
2019-04-30 21:07:37 +0800

    Create 049.py

---
+++ code/049.py
@@ -0,0 +1,17 @@
+# using range
+
+my_range = range(1,5)
+print(type(my_range))
+
+print(my_range)
+
+for x in my_range:
+    print(x)
+
+# Output:
+# <class 'range'>
+# range(1, 5)
+# 1
+# 2
+# 3
+# 4

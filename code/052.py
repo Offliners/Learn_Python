d1367685d70c4447dfff9edc9fd608b64b6e5340
Offliner <42777325+Offliners@users.noreply.github.com>
2019-04-30 22:47:10 +0800

    Create 052.py

---
+++ code/052.py
@@ -0,0 +1,15 @@
+name = "Eric"
+print("Hello, " + name + "!")
+
+# using string formatting
+
+# old style
+print("Hello, %s!" % name)
+
+# new style
+print("Hello, {}!".format(name))
+
+# Output:
+# Hello, Eric!
+# Hello, Eric!
+# Hello, Eric!

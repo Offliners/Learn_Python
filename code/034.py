99746c89573c1ba3cfd545d83c7d5fa979f5b04f
Offliner <42777325+Offliners@users.noreply.github.com>
2019-04-30 17:49:07 +0800

    Create 034.py

---
+++ code/034.py
@@ -0,0 +1,19 @@
+# using "in"
+
+# with string
+print("s" in "Water")
+print("s" in "Wasser")
+
+# with lists
+my_list = [1,2,3,4,5]
+print(4 in my_list)
+
+# with sets
+my_set = {1,2,3,4,5}
+print(4 in my_set)
+
+# Output:
+# False
+# True
+# True
+# True

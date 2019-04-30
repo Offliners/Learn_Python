f98e3e58401356752aad6e5f6702772eba9264e1
Offliner <42777325+Offliners@users.noreply.github.com>
2019-04-30 14:56:42 +0800

    Create 020.py

---
+++ code/020.py
@@ -0,0 +1,12 @@
+# tuples vs lists
+# similar but different
+
+var1 = [2,4,6,8] # list
+var2 = (2,4,6,8) # tuple
+
+print(type(var1))
+print(type(var2))
+
+# Output:
+# <class 'list'>
+# <class 'tuple'>

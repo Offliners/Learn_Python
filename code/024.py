8d4c6ec07c97b1ffd3fe6a1e7c6f8e1dbdec1510
Offliner <42777325+Offliners@users.noreply.github.com>
2019-04-30 15:05:27 +0800

    Create 024.py

---
+++ code/024.py
@@ -0,0 +1,14 @@
+# using the third parameter
+# in a range
+
+str = "abcdefghijklmn"
+
+# print every second character
+print(str[::2])
+
+# print every third character
+print(str[::3])
+
+# Output:
+# acegikm
+# adgjm

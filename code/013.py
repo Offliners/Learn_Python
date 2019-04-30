1aeb619d6e20de01b49325ed9dc994ae0696a259
Offliner <42777325+Offliners@users.noreply.github.com>
2019-04-30 14:39:00 +0800

    Create 013.py

---
+++ code/013.py
@@ -0,0 +1,12 @@
+# getting a substring
+
+str = "Hello, World!"
+#      0123456789012
+#             ^^^^^
+# I want to print "World"
+
+str_World = str[7:12]
+print(str_World) 
+
+# Output:
+# World

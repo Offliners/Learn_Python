c0837798cff4c06ce64059ca9cacb4d44ccab838
Offliner <42777325+Offliners@users.noreply.github.com>
2019-04-30 20:59:00 +0800

    Create 045.py

---
+++ code/045.py
@@ -0,0 +1,15 @@
+# the "else" clause can be 
+# used together with "while"
+
+# if the main condition
+# is never met
+
+x = 100
+while x < 5:
+    print(x)
+    x += 1
+else:
+    print("oops")
+
+# Output:
+# oops

ccaf403de625ed819b80197734b3dc295a70a547
Offliner <42777325+Offliners@users.noreply.github.com>
2019-04-30 22:58:43 +0800

    Create 056.py

---
+++ code/056.py
@@ -0,0 +1,15 @@
+# examining the third parameter of 
+# ranges
+
+r = range(1,10,2)
+#              ^ 2 is a step
+
+# let's convert it to a list
+# and print the result
+
+a = list(r)
+print(a)
+# every second number will be printed
+
+# Output:
+# [1, 3, 5, 7, 9]

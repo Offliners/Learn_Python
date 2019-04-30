c54f1948d5d3ed88de9e2dcf6c0bab17975508ea
Offliner <42777325+Offliners@users.noreply.github.com>
2019-04-30 22:55:29 +0800

    Create 055.py

---
+++ code/055.py
@@ -0,0 +1,14 @@
+# when the new style of string
+# formatting can be very handy
+
+a,b = 10,20
+print("a + b = b + a")
+print("{0} + {1} = {1} + {0}".format(a,b))
+
+# {0} is replaced with a
+# {1} is replaced with b
+# but you only mention then once
+
+# Output:
+# a + b = b + a
+# 10 + 20 = 20 + 10

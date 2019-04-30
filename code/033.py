d2e23295ab480f932ef7d51dd7e1597af693f634
Offliner <42777325+Offliners@users.noreply.github.com>
2019-04-30 17:46:21 +0800

    Create 033.py

---
+++ code/033.py
@@ -0,0 +1,30 @@
+# using if, else, and elif
+
+x = int(input("Enter x : "))
+
+if x < 5:
+    print("Less than 5")
+elif x < 10:
+    print("Less than 10")
+else:
+    print("10 or more")
+
+# Input:
+# 10
+# Output:
+# 10 or more
+
+# Input:
+# 20
+# Output:
+# 10 or more
+
+# Input:
+# 7
+# Output:
+# Less than 10
+
+# Input:
+# 2
+# Output:
+# Less than 5

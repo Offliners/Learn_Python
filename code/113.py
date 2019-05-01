731aa4f2869009c71cb18dc119ad566f05b3eedd
Offliner <42777325+Offliners@users.noreply.github.com>
2019-05-01 23:59:07 +0800

    Create 113.py

---
+++ code/113.py
@@ -0,0 +1,12 @@
+# you have some module, and
+# you want to see if you are
+# importing it from a correct
+# location
+
+import mylib
+
+print(mylib.__file__)
+# print the path to the module
+
+# Output:
+# c:\Users\ASUS\Desktop\Python\mylib.py

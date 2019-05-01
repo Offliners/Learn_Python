4760054ccada41c3fee3df1c2fb790a9b7423b60
Offliner <42777325+Offliners@users.noreply.github.com>
2019-05-01 23:46:57 +0800

    Create 108.py

---
+++ code/108.py
@@ -0,0 +1,14 @@
+# looking at the loop variable
+# after the loop is over
+
+colours = ["red","green","blue"]
+for c in colours:
+    print(c)
+
+print("After the loop: " + c)
+
+# Output:
+# red
+# green
+# blue
+# After the loop: blue

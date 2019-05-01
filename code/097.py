450cd6c6cb875132fc59b3fc598b05ef19d76896
Offliner <42777325+Offliners@users.noreply.github.com>
2019-05-01 18:00:00 +0800

    Create 097.py

---
+++ code/097.py
@@ -0,0 +1,8 @@
+# let us simplify the code
+# by using the "with" keyword.
+# write to a text file
+
+with open("Data/t2.txt","w") as f:
+    f.write("Hello, World!")
+
+# it will be closed automatically

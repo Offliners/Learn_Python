c7ced2ec8e1073990b062e1876e6d6250a785ff7
Offliner <42777325+Offliners@users.noreply.github.com>
2019-05-01 23:41:56 +0800

    Create 106.py

---
+++ code/106.py
@@ -0,0 +1,18 @@
+# using decorator to measure time
+
+import time
+
+def time_measure(f): # f = function
+    t0 = time.time()
+    f() # call that function
+    t = time.time()
+    return t - t0 # duration
+
+@time_measure # decorator
+def slow_code():
+    time.sleep(2)
+
+print(slow_code) # no ()
+
+# Output:
+# 2.000826835632324

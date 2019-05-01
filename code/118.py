e84e4075109e89659a97b15e7f1d0bda1bac76b7
Offliner <42777325+Offliners@users.noreply.github.com>
2019-05-02 00:16:56 +0800

    Create 118.py

---
+++ code/118.py
@@ -0,0 +1,13 @@
+# raw strings in Python
+
+# regular string:
+print("Hello, \nWorld!")
+
+# "raw" string
+# prefixed with "r"
+print(r"Hello, \nWorld!")
+
+# Output:
+# Hello,
+# World!
+# Hello, \nWorld!

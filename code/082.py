a5d85c8f1f0f78ed19ec3c8c789e5d019794352c
Offliner <42777325+Offliners@users.noreply.github.com>
2019-05-01 17:04:55 +0800

    Create 082.py

---
+++ code/082.py
@@ -0,0 +1,24 @@
+# a simple "echo"
+# program
+
+while True:
+    try:
+        s = input()
+        print(s)
+    except:
+        break
+        # now the program will
+        # simply stop after
+        # the end of input
+
+# Input:
+# Hello,
+# World!
+# Pressing ^C
+# ^C
+
+# Output:
+# Hello,
+# World!
+# Pressing ^C
+#

3dfaaeaa7acbf17cae6e88ebd78d4a6ee30d9af1
Offliner <42777325+Offliners@users.noreply.github.com>
2019-05-01 23:35:11 +0800

    Create 105.py

---
+++ code/105.py
@@ -0,0 +1,16 @@
+# measure the time of
+# a slow piece of code
+
+import time
+
+def slow_code():
+    time.sleep(2)
+
+t0 = time.time() # start time
+slow_code()
+t = time.time() # end time
+
+print(t - t0)
+
+# Output:
+# 2.0002713203430176

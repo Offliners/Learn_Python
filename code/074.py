1142f4723f6eec4c86fc0c8f5291a0b36bc8eeb3
Offliner <42777325+Offliners@users.noreply.github.com>
2019-05-01 15:23:13 +0800

    Create 074.py

---
+++ code/074.py
@@ -0,0 +1,17 @@
+def my_f(a,b):
+    return a**b
+
+# with named arguments, you
+# can change their order when
+# calling a function
+
+print(my_f(2,3))
+print(my_f(a=2,b=3))
+
+# different order
+print(my_f(b=2,a=3))
+
+# Output:
+# 8
+# 8
+# 9

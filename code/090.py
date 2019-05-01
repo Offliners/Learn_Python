4d2330eaa6360fe913d94d608a19fda6d1ac0db7
Offliner <42777325+Offliners@users.noreply.github.com>
2019-05-01 17:40:07 +0800

    Create 090.py

---
+++ code/090.py
@@ -0,0 +1,14 @@
+# custom object formatting
+
+class C:
+    def __init__(self,value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+c = C(42)
+print(c) # How?? # And now?  
+
+# Output:
+# 42

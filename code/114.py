7d5c14a5ada7cfa3708c17a5bdce5721da08434f
Offliner <42777325+Offliners@users.noreply.github.com>
2019-05-02 00:02:36 +0800

    Create 114.py

---
+++ code/114.py
@@ -0,0 +1,19 @@
+# count how many times each item
+# appears in the list
+
+my_list = ["a","b","a","b","c",
+           "b","d","b","e","f"]
+
+for item in sorted(set(my_list)):
+    # set(my_list): each item appears
+    # only one time
+    count = my_list.count(item)
+    print(f"item = {count} times")
+
+# Output:
+# item = 2 times
+# item = 4 times
+# item = 1 times
+# item = 1 times
+# item = 1 times
+# item = 1 times

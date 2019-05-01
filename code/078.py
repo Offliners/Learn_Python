1a2ef68cc747840e5fb57ed421e3621a024090c1
Offliner <42777325+Offliners@users.noreply.github.com>
2019-05-01 15:35:32 +0800

    Create 078.py

---
+++ code/078.py
@@ -0,0 +1,13 @@
+# computing a factorial
+# using recursion
+
+def factorial(n):
+    if n > 1:
+        return n * factorial(n - 1)
+    else:
+        return 1
+
+print(factorial(5))
+
+# Output:
+# 120

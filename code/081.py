912a07ef8b1adbf4b0e3d6192aa80464c1f59d50
Offliner <42777325+Offliners@users.noreply.github.com>
2019-05-01 17:01:19 +0800

    Create 081.py

---
+++ code/081.py
@@ -0,0 +1,20 @@
+# a simple "echo"
+# program
+
+while True:
+    s = input()
+    print(s)
+
+# Input:
+# Hello
+# World!
+# ^c # Interrupt
+
+# Output:
+# Hello
+# World!
+# Traceback (most recent call last):
+#   File "c:/Users/ASUS/Desktop/Python/081.py", line 5, in
+# <module>
+#     s = input()
+# KeyboardInterrupt

058006a1da8983e7fb8a042a2ee16ed7a5eb1bf7
Offliner <42777325+Offliners@users.noreply.github.com>
2019-05-01 13:41:01 +0800

    Create 063.py

---
+++ code/063.py
@@ -0,0 +1,16 @@
+# regular function
+# def sqr(x):
+#     return x ** 2
+
+# lambda (inline function)
+sqr = lambda x : x ** 2
+
+for i in range(5):
+    print(sqr(i))
+
+# Output:
+# 0
+# 1
+# 4
+# 9
+# 16

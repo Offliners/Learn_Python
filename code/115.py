950af38f4281e09166beacd64a0726f5d8ad7b38
Offliner <42777325+Offliners@users.noreply.github.com>
2019-05-02 00:05:58 +0800

    Create 115.py

---
+++ code/115.py
@@ -0,0 +1,20 @@
+# splitting data into
+# odd and even numbers
+
+data = [45,56,67,23,57,78,67,454]
+
+odd = []
+even = []
+
+for d in data:
+    if d % 2:
+        odd.append(d)
+    else:
+        even.append(d)
+
+print(f"Odd numbers: {odd}")
+print(f"Even numbers: {even}")
+
+# Output:
+# Odd numbers: [45, 67, 23, 57, 67]
+# Even numbers: [56, 78, 454]

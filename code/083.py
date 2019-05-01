84a1ccdbed61820e24ca1f535417599b6e5ef8e6
Offliner <42777325+Offliners@users.noreply.github.com>
2019-05-01 17:10:09 +0800

    Create 083.py

---
+++ code/083.py
@@ -0,0 +1,13 @@
+# reading from a text file
+
+for str in open("Data/data.txt"):
+    print(str, end="")
+
+# we should not print the
+# newline after each string,
+# as "str" already contains it
+
+# Output:
+# line 1
+# line 2
+# line 3

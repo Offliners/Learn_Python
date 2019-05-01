5f3bcc97507536384f8d0719efb06a803f2dcf40
Offliner <42777325+Offliners@users.noreply.github.com>
2019-05-01 17:54:02 +0800

    Create 095.py

---
+++ code/095.py
@@ -0,0 +1,12 @@
+# write to a text file
+
+f = open("Data/t.txt","w")
+# or:
+# f = open("Data/t.txt",mode = "w")
+# w = create and write
+# r = read(default)
+# a = append
+# x = create if not existed
+
+f.write("Hello, World!")
+f.close()

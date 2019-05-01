5847c4475e9df0dcf34a9e9abaf29edc1441f1a9
Offliner <42777325+Offliners@users.noreply.github.com>
2019-05-01 17:27:57 +0800

    Create 086.py

---
+++ code/086.py
@@ -0,0 +1,19 @@
+# creating a class in Python
+
+class Animal:
+    def talk(self):
+        print("I am an animal")
+
+# creating an object or an class
+
+Animal1 = Animal()
+Animal1.talk()
+
+# another object of the same class
+
+Animal2 = Animal()
+Animal2.talk()
+
+# Output:
+# I am an animal
+# I am an animal

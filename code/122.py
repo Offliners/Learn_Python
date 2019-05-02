3d1b77641609e33a7c9a616daebf0409bc1d56bd
Offliner <42777325+Offliners@users.noreply.github.com>
2019-05-02 23:17:33 +0800

    Create 122.py

---
+++ code/122.py
@@ -0,0 +1,12 @@
+# printing some basic information
+# about the operating system
+# you are currently working with
+
+import sys
+
+print(sys.platform)
+print(sys.version_info)
+
+# Output:
+# win32
+# sys.version_info(major=3, minor=6, micro=8, releaselevel='final', serial=0)

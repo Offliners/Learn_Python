17f19ef65d7d80cbf3b4863fd8bc9b4678c0c8c9
Offliner <42777325+Offliners@users.noreply.github.com>
2019-05-06 22:49:25 +0800

    Create 126.py

---
+++ code/126.py
@@ -0,0 +1,17 @@
+# getting keys and values
+# of a dictionary
+
+data = {"France" : "Paries",
+        "Italy" : "Rome",
+        "Germany" : "Berlin"}
+
+print(data.keys())
+print(data.values())
+
+# values will correspond to the keys
+# in the output : they appear in
+# the same order
+
+# Output:
+# dict_keys(['France', 'Italy', 'Germany'])
+# dict_values(['Paries', 'Rome', 'Berlin'])

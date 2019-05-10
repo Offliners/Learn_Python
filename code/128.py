7e4636ed580d617505c3396c4f4f124e091f32aa
Offliner <42777325+Offliners@users.noreply.github.com>
2019-05-10 23:07:03 +0800

    Create 128.py

---
+++ code/128.py
@@ -0,0 +1,21 @@
+# how to push data to a list
+# and how to pop it back
+
+data = []
+
+for i in range(5):
+    data.append(i * i)
+    # fill with some data
+
+while(len(data)):
+    # while list is not empty
+    value = data.pop() # take from back
+    print(value)
+    # printed in reverse order
+
+# Output:
+# 16
+# 9
+# 4
+# 1
+# 0

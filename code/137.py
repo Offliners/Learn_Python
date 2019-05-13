ed84d1bcb32fe32bc81005dbc52cb3e82eda10f7
Offliner <42777325+Offliners@users.noreply.github.com>
2019-05-13 13:54:57 +0800

    Create 137.py

---
+++ code/137.py
@@ -0,0 +1,16 @@
+# remove duplicates from a list
+# and make sure the order is saved
+
+data = [10,12,10,33,2,2,5,2]
+result = []
+
+values = set()
+for i in data:
+    if i not in values:
+        values.add(i)
+        result.append(i)
+
+print(result)
+
+# Output:
+# [10, 12, 33, 2, 5]

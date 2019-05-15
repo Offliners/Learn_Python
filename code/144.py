8a11aada9d744558106fb24732cfcb32f185b193
Offliner <42777325+Offliners@users.noreply.github.com>
2019-05-15 17:41:14 +0800

    Create 144.py

---
+++ code/144.py
@@ -0,0 +1,14 @@
+# let's see another paradigm
+# you can use with conditional
+# statements
+
+for i in range(1,5):
+    # is i odd or even?
+    print("odd") if i % 2 else print("even")
+    
+
+# Output:
+# odd
+# even
+# odd
+# even

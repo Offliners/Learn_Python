d1e8d602b3d92cd74763ae80ad2dc1da1b05c66a
Offliner <42777325+Offliners@users.noreply.github.com>
2019-05-15 17:33:13 +0800

    Create 141.py

---
+++ code/141.py
@@ -0,0 +1,25 @@
+# generating and printing
+# fibonacci numbers
+# below 100
+
+a,b = 0,1
+while a < 100:
+    print(a)
+    a,b = b,a+b
+
+# notice how nice it
+# multiple assigment
+
+# Output:
+# 0
+# 1
+# 1
+# 2
+# 3
+# 5
+# 8
+# 13
+# 21
+# 34
+# 55
+# 89

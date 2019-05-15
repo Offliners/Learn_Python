92e0adaf54020d295ad1c45abf66ad13ddb4c3ed
Offliner <42777325+Offliners@users.noreply.github.com>
2019-05-15 17:38:55 +0800

    Create 143.py

---
+++ code/143.py
@@ -0,0 +1,13 @@
+# need a ternary operator?
+# use conditional expression
+
+for i in range(1,5):
+    # is i odd or even?
+    oe = "odd" if i % 2 else "even"
+    print(f"{i} is {oe}")
+
+# Output:
+# 1 is odd
+# 2 is even
+# 3 is odd
+# 4 is even

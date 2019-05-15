ab0e80a9b5274c113e3fa5fefc0f0de36cd2393c
Offliner <42777325+Offliners@users.noreply.github.com>
2019-05-15 17:36:57 +0800

    Create 142.py

---
+++ code/142.py
@@ -0,0 +1,17 @@
+# a safer way of reading values from 
+# a dictionary
+
+data = {"France" : "Paris",
+        "Italy" : "Rome"}
+
+# capital = data["Germany"] # error
+capital = data.get("Germany") # OK
+# but it returns "None"
+print(capital)
+
+capital = data.get("Germany","??")
+print(capital) # prints "??"
+
+# Output:
+# None
+# ??

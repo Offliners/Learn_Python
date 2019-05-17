3148c439a1f79ea4de43db17c330ecdacaec80e3
Offliner <42777325+Offliners@users.noreply.github.com>
2019-05-17 17:41:32 +0800

    Create 147.py

---
+++ code/147.py
@@ -0,0 +1,14 @@
+# using "enumerate"
+
+data = ["alpha","beta","gamma"]
+
+for index,value in enumerate(data):
+    # index = number of the element
+    # in the list
+    # value = the item itself
+    print(f"The element #{index} " f"is {value}")
+
+# Output:
+# The element #0 is alpha
+# The element #1 is beta
+# The element #2 is gamma

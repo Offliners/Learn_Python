80ed01cee917a5d8e4445a21c38afead8352c792
Offliner <42777325+Offliners@users.noreply.github.com>
2019-05-21 15:23:55 +0800

    Create 156.py

---
+++ code/156.py
@@ -0,0 +1,18 @@
+# the "for" loop can have an 
+# "else" clause
+
+for i in range(5):
+    print(i)
+    if i == 3:
+        break
+else:
+    print("done") 
+    # printed after the end of the loop
+    # but only if the loop finished
+    # successfully
+ 
+# Output:
+# 0
+# 1
+# 2
+# 3

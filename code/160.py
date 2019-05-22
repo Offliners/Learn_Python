f3e766c9ebaa4c3cc30525f9f5ce8447758bc234
Offliner <42777325+Offliners@users.noreply.github.com>
2019-05-22 13:55:37 +0800

    Create 160.py

---
+++ code/160.py
@@ -0,0 +1,14 @@
+# a small difference between
+# python 2 and python 3
+
+message = "Hello, World!"
+
+# print message
+# works in python 2
+# does not work in python 3
+
+# you need parenthese:
+print(message)
+
+# Output:
+# Hello, World!

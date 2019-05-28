4b589b786031717b8e2ba4c70101021ff3e047e5
Offliner <42777325+Offliners@users.noreply.github.com>
2019-05-28 17:58:29 +0800

    Create 168.py

---
+++ code/168.py
@@ -0,0 +1,18 @@
+# string literal concatenation
+
+# you have a long string and want
+# to put it into a small width of
+# your editor's window
+s = "Hello, " "World!"
+# you can split it in two parts
+print(s)
+
+# or even into a few lines
+s = "Hello, " \
+    "World" \
+    "!"
+print(s)
+
+# Output:
+# Hello, World!
+# Hello, World!

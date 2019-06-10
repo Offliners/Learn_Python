dd6b644f9fb1142147b59fb83ce31e813716221b
Offliner <42777325+Offliners@users.noreply.github.com>
2019-06-10 23:51:36 +0800

    Create 200.py

---
+++ code/200.py
@@ -0,0 +1,19 @@
+# find the (only) duplicate
+# number in the given list
+# of integers
+
+data = [3,4,7,8,1,2,4,9]
+# "4" to be found
+
+seen = set()
+for x in data:
+    if x in seen:
+        # if we already met x
+        print(x)
+        break
+    seen.add(x) # keep collecting
+else:
+    print("No duplicates found")
+
+# Output:
+# 4

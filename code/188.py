dc94f254a25f20acc50bedc107438619ec1e9c75
Offliner <42777325+Offliners@users.noreply.github.com>
2019-06-10 23:08:04 +0800

    Create 188.py

---
+++ code/188.py
@@ -0,0 +1,15 @@
+# copying lists
+
+data = [3,5,7,9]
+
+data2 = data # the same list
+data3 = data.copy() # new list
+
+data2.insert(1,199) # changes both data and data2
+
+data3.insert(1,299) # only changes data3. data stays unchanged
+
+print(data)
+
+# Output:
+# [3, 199, 5, 7, 9]

888c285822369952b0f7d8345ecbf0f64f8fd406
Offliner <42777325+Offliners@users.noreply.github.com>
2019-06-10 23:01:34 +0800

    Create 185.py

---
+++ code/185.py
@@ -0,0 +1,16 @@
+# how many bits does your integer
+# need in memory
+
+i = 34
+print(i.bit_length())
+
+i = 340
+print(i.bit_length())
+
+i = 34238459238539284329572395832
+print(i.bit_length())
+
+# Output:
+# 6
+# 9
+# 95

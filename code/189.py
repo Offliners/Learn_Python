1237a40484b4d2c0b4369e51a771607526bfe954
Offliner <42777325+Offliners@users.noreply.github.com>
2019-06-10 23:11:07 +0800

    Create 189.py

---
+++ code/189.py
@@ -0,0 +1,15 @@
+# how to create a tuple with
+# only one element
+
+x = ("value") # is it a tuple?
+print(type(x)) # it is a string
+
+y = ("value",) # this comma makes it
+# a tuple (because the compiler sees that
+# you kind of want more elements here)
+
+print(type(y)) # tuple
+
+# Output:
+# <class 'str'>
+# <class 'tuple'>

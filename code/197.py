bd962123782d619ebe710b8eddb71c322eb7ed6a
Offliner <42777325+Offliners@users.noreply.github.com>
2019-06-10 23:41:59 +0800

    Create 197.py

---
+++ code/197.py
@@ -0,0 +1,18 @@
+# find a missing number in a list
+data = [3,8,1,9,4,10,6,7,2]
+# 5 is missing; list is not sorted
+
+# you can also transform a list to 
+# a set to make the solution faster
+# for bigger data arrays:
+
+data = set(data)
+for i in range(1,len(data)+1):
+    # 1 ... 10
+    if i not in data:
+        print(f"{i} is missing")
+        # done; it's found
+        break
+
+# Output:
+# 5 is missing

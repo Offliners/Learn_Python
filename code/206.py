041fd4eee0c06ef66816e016f0b44fd58f23b85c
Offliner <42777325+Offliners@users.noreply.github.com>
2019-06-17 00:55:20 +0800

    Create 206.py

---
+++ code/206.py
@@ -0,0 +1,22 @@
+# define a "+" operator for
+# your own class
+
+class C:
+    def __init__(self,value):
+        self.value = value
+
+    def __add__(self,x):
+        return self.value + x
+
+    def __radd__(self,x):
+        return self.value + x
+
+obj = C(5)
+print(obj + 10) # OK, used __add__
+
+# what if you want to do it this way:
+print(10 + obj) # OK, used __radd__
+
+# Output:
+# 15
+# 15

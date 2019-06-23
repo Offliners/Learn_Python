b73b72bde428d5a8485117df54f1d92a58feddbd
Offliner <42777325+Offliners@users.noreply.github.com>
2019-06-23 11:41:28 +0800

    Create 220.py

---
+++ code/220.py
@@ -0,0 +1,16 @@
+# implement the "sum" function
+# recursively (as an exercise)
+
+def rs(data):
+    # data is a list
+    if len(data) > 1:
+        return data[0] + rs(data[1:])
+        #                ^^
+        # here comes recursion
+    else:
+        return data[0]
+
+print(rs([4,5,7,9,10]))
+
+# Output:
+# 35

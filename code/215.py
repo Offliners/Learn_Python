8f5fd94f3f6ebcbc1597c0978694ae54d0a83c02
Offliner <42777325+Offliners@users.noreply.github.com>
2019-06-23 11:24:38 +0800

    Create 215.py

---
+++ code/215.py
@@ -0,0 +1,19 @@
+# how many digits are there in 
+# a number
+
+import math
+
+n = 123456
+
+# method 1 
+print(len(str(n)))
+# convert a number to a string
+# and take its length
+
+# method 2 (pure mathematical)
+print(1 + math.floor(math.log10(n)))
+# log - base 10 
+
+# Output:
+# 6
+# 6

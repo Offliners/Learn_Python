ea83d46cd6f1b177269543b256bb2a5137e7798f
Offliner <42777325+Offliners@users.noreply.github.com>
2019-06-23 11:53:52 +0800

    Create 224.py

---
+++ code/224.py
@@ -0,0 +1,13 @@
+# reading a password from
+# the keyboard
+
+import getpass
+
+pwd = getpass.getpass("Your pwd : ")
+print(pwd) # just to check if that works
+
+# Input:
+# helloworld
+
+# Output:
+# helloworld

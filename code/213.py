92e05ebccc670ecc5894e941a94aab8608b9796f
Offliner <42777325+Offliners@users.noreply.github.com>
2019-06-23 11:12:42 +0800

    Create 213.py

---
+++ code/213.py
@@ -0,0 +1,18 @@
+# positional-only parameters
+# available in python 3.8+
+
+def s(a,b,/): # notice the /
+    print(a+b)
+
+s(10,20) # OK
+# s(a=10,b=20) # error
+
+def s2(a,b,/,c):
+    # c can be named
+    print(a+b+c)
+
+s2(10,20,c=30)
+
+# Output:
+# 30
+# 60

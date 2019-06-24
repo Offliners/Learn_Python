fc3763e33d44bb8ec288aa5e80fece520e90b399
Offliner <42777325+Offliners@users.noreply.github.com>
2019-06-24 17:12:49 +0800

    Create 225.py

---
+++ code/225.py
@@ -0,0 +1,18 @@
+# using a temporary file 
+# (and reading and writing)
+
+import tempfile
+
+def read_data(f):
+    f.seek(0)
+    data = f.read()
+    print(data)
+
+f = tempfile.TemporaryFile()
+f.write(b"Some temp data") # binary str
+
+read_data(f)
+f.close()
+
+# Output:
+# b'Some temp data'

d5f4413278f0abd85ce28c55f5cbc514fd531c59
Offliner <42777325+Offliners@users.noreply.github.com>
2019-06-24 17:14:07 +0800

    Create 226.py

---
+++ code/226.py
@@ -0,0 +1,19 @@
+# generating random UUIDs
+
+import uuid
+
+for _ in range(10):
+    u = uuid.uuid4()
+    print(u)
+
+# Output:
+# 835d8793-2fe9-4db3-ba6a-20bb701007ed
+# 5c3eca80-490e-41c4-bf39-4ca1501c4dd0
+# c5001bd3-f2bb-4ba9-a809-22f751b3d22c
+# dcbb6dda-91d6-49ea-95b0-4eb92351aad7
+# d2d2e76f-3ece-4193-bef9-7e3211ae84f7
+# 3c270436-81eb-4c86-b99d-768dbc01ee6e
+# bfcdc418-088d-4556-9687-6ab7beb4e9d3
+# f9b11c20-b93e-4b9b-8294-68b2bea122d2
+# 05ddc320-a49b-4cd7-9636-b1b5d3e89936
+# aaecd849-0232-452e-971b-a7a201c5484b

22ef77297f76145801db9dbaf5c9dec0cff9cc51
Offliner <42777325+Offliners@users.noreply.github.com>
2019-07-01 22:38:46 +0800

    Create 237.py

---
+++ code/237.py
@@ -0,0 +1,12 @@
+# another method of calculating
+# the average value : using
+# modules
+
+import statistics
+
+data = [10,40,55,77,88,15]
+avg = statistics.mean(data)
+print(avg)
+
+# Output:
+# 47.5

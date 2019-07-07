cdd42e6a22d8ee40548ef57de8bd74bc768524ad
Offliner <42777325+Offliners@users.noreply.github.com>
2019-07-07 10:49:11 +0800

    Create 244.py

---
+++ code/244.py
@@ -0,0 +1,22 @@
+# using the monte carlo method
+# to compute the value of pi
+# (approximately)
+
+import random
+import math
+
+n = 100000
+inside = 0
+for _ in range(n):
+    x = random.uniform(-1,1) # between -1,1
+    y = random.uniform(-1,1) 
+    d = math.sqrt(x**2 + y**2) # distance
+    # from the center of a circle
+    if d <= 1:
+        inside += 1
+
+pi = 4 * inside / n
+print(pi)
+
+# Output
+# 3.14164
